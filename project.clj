49d6121e881bd35bc0a14496cbcf99cf04974edd
Yehonathan Sharvit <viebel@gmail.com>
2016-08-14 14:18:29 +0300

    missing files

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject viebel/lambda-interpreter "0.0.1"
+  :description "Lambda Calculus interpreter"
+  :url "https://github.com/viebel/lambda-calculus-interpreter-clj"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [midje "1.8.3"]]
+  :plugins [[lein-midje "3.2"]]
+  :source-paths ["src"])
